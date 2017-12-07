47f345a3ee76957410e230e6a2398d9511778f01
Ali <30610753+aamining@users.noreply.github.com>
2017-12-07 14:24:18 +1100

    mongo research

--- api/models/person.js
+++ api/models/person.js
@@ -1,9 +1,11 @@
 const mongoose = require('./base');
 
+// override so it creates within the "movies" db, a collection
+// called "person" (not "people" which is what mongo creates automatically)
 const PersonSchema = mongoose.Schema({
   firstName: String,
   lastName: String
-});
+}, { collection: "person" });
 
 const Person = mongoose.model('Person', PersonSchema);
 
